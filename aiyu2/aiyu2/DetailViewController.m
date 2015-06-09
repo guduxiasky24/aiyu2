7511fda9ef02c8fd2625c1b53652bb19c037f18b
吴金林 <kolin@wujinlindeMacBook-Pro.local>
2015-06-09 16:11:40 +0800

    2015-6-9 项目建设

---
+++ aiyu2/aiyu2/DetailViewController.m
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.m
+//  aiyu2
+//
+//  Created by 吴金林 on 15/6/9.
+//  Copyright (c) 2015年 kolin. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem {
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+            
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView {
+    // Update the user interface for the detail item.
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ aiyu2/aiyu2/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  aiyu2
+//
+//  Created by 吴金林 on 15/6/9.
+//  Copyright (c) 2015年 kolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
